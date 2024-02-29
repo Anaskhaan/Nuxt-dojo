f29f0ee21053a72bc5516202904e1de0a4cd66e8
Anaskhaan <ak4066912@gmail.com>
2024-02-29 17:52:24 +0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi veniam
+      molestiae ab! Ullam ducimus tempore omnis! Distinctio, libero facilis?
+      Vitae.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa nisi id
+      accusantium facilis ullam commodi voluptates et quidem deserunt aut.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
